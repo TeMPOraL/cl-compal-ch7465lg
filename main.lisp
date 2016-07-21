2f3033415c9b3dcaa742ff72d908181554076f47
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2016-07-21 17:55:12 +0200

    Sketching the API.

--- main.lisp
+++ main.lisp
@@ -2,5 +2,18 @@
 
 ;;; TODO.
 
-(define-constant +function-list-connected-devices+ 123)
+(defparameter *modem-ip* "192.168.0.1")
+(defparameter *modem-pin* "12345")
 
+(define-constant +function-list-connected-devices+ 123)
+(define-constant +getter+ "getter.xml" :test #'string=)
+(define-constant +setter+ "setter.xml" :test #'string=)
+
+(defun device-info ()
+  ;; TODO do a request
+  )
+
+(defun list-devices ()
+  ;; TODO log in if not logged in
+  ;; TODO do a request
+  )
